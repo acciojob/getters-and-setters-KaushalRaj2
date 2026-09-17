27b0b17da339d247bb70c3d37c2b6b85e0048c1f
Rajeev Kumar Kaushal <165012448+KaushalRaj2@users.noreply.github.com>
2026-09-17 17:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name:string,age:number){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(age:number){
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
 
-class Teacher extends Person {}
+	constructor(name,age,study){
+		super(name,age);
+		this.study=study;
+	}
+
+	study(){
+		return this.name " is studying"
+	}
+	
+}
+
+class Teacher extends Person {
+
+	constructor(name,age,teach){
+		super(name,age);
+		this.teach=teach;
+	}
+
+	teach(){
+		return this.name" is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
